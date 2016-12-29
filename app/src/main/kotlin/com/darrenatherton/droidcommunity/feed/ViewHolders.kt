c374feb87d0306e1293d7264b95428f791984e8b
darrenatherton <darren.atherton49@yahoo.com>
2016-12-29 21:59:19 +0000

    Adds basic reddit feed item view

--- app/src/main/kotlin/com/darrenatherton/droidcommunity/feed/ViewHolders.kt
+++ app/src/main/kotlin/com/darrenatherton/droidcommunity/feed/ViewHolders.kt
@@ -3,13 +3,17 @@ package com.darrenatherton.droidcommunity.feed
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.darrenatherton.droidcommunity.feed.entity.FeedViewItem
+import kotlinx.android.synthetic.main.item_feed_reddit.view.*
 
 sealed class FeedListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     class RedditItemViewHolder(itemView: View) : FeedListViewHolder(itemView) {
 
         fun bind(item: FeedViewItem.Reddit) {
-
+            with(item) {
+                itemView.title_textview.text = title
+                itemView.subreddit_textview.text = title
+            }
         }
     }
 
